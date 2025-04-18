92555c62bb964d85ec351405bc550bd359cad3aa
Amirali Mansouri <amiralimansouri98@outlook.com>
2025-04-18 21:35:41 +0330

    Some basic configurations.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import { ThemeProvider } from "@emotion/react";
+import "./styles/index.css";
+import "./styles/fonts.css";
+import App from "./App.jsx";
+import theme from "./mui/theme.js";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  <ThemeProvider theme={theme}>
     <App />
-  </StrictMode>,
-)
+  </ThemeProvider>
+);
