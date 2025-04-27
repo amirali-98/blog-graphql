f16f8d99a789969840397d8731e40031823d3ef6
Amirali Mansouri <amiralimansouri98@outlook.com>
2025-04-27 19:57:32 +0330

    Add loader for post's page.

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
-import { Container, Typography } from "@mui/material";
+import { Container, Typography, Box } from "@mui/material";
 import dompurify from "dompurify";
+import { BounceLoader } from "react-spinners";
 import { GET_POST } from "../graphql/queries";
 import CommentForm from "../components/CommentForm";
 import CommentList from "../components/CommentList";
@@ -24,32 +25,48 @@ export default function Post() {
       loading: response.loading,
     });
   }, [response]);
-  console.log(response);
 
   return (
     <Container maxWidth="lg">
-      <Typography
-        component="h1"
-        variant="h5"
-        fontWeight="700"
-        color="#111827"
-        mb={2}
-      >
-        {post.title}
-      </Typography>
-      <img
-        style={{ width: "100%", borderRadius: "16px", marginBottom: "16px" }}
-        src={post.cover}
-        alt={post.title}
-      />
-      <Typography
-        className="post-content"
-        dangerouslySetInnerHTML={{
-          __html: dompurify.sanitize(post.content),
-        }}
-      />
-      <CommentForm slug={slug} />
-      <CommentList comments={post.comments} />
+      {true ? (
+        <Box
+          height="80vh"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <BounceLoader color="#1976D2" size={200} />
+        </Box>
+      ) : (
+        <>
+          <Typography
+            component="h1"
+            variant="h5"
+            fontWeight="700"
+            color="#111827"
+            mb={2}
+          >
+            {post.title}
+          </Typography>
+          <img
+            style={{
+              width: "100%",
+              borderRadius: "16px",
+              marginBottom: "16px",
+            }}
+            src={post.cover}
+            alt={post.title}
+          />
+          <Typography
+            className="post-content"
+            dangerouslySetInnerHTML={{
+              __html: dompurify.sanitize(post.content),
+            }}
+          />
+          <CommentForm slug={slug} />
+          <CommentList comments={post.comments} />
+        </>
+      )}
     </Container>
   );
 }
